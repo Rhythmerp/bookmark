32daa47252967d8c03a98f17c0ad8686fff60b8a
qwevvv2003 <qwevvv2003@naver.com>
2019-03-26 01:18:17 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static_files')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
